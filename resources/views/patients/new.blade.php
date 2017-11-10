7faafdfc91887198f749df603e961b915c74b608
fr4j4 <fgonz@outlook.com>
2017-11-10 10:25:33 -0300

    pacientes edad

--- resources/views/patients/new.blade.php
+++ resources/views/patients/new.blade.php
@@ -2,7 +2,7 @@
 @section('title','Registrar nuevo paciente')
 @section('content')
 	<div class="container">
-
+<a href="{{route('patients_index')}}" type="button" class="btn btn-primary"><i class="fa fa-arrow-left" aria-hidden="true"></i> Cancelar y volver a lista de pacientes</a>
                 <div class="x_panel">
   
                   <div class="x_content">
@@ -26,7 +26,7 @@
                             <label class="btn btn-default" data-toggle-class="btn-primary" data-toggle-passive-class="btn-default">
                               <input type="radio" name="gender" value="male"> &nbsp; Mujer &nbsp;
                             </label>
-                            <label class="btn btn-primary" data-toggle-class="btn-primary" data-toggle-passive-class="btn-default">
+                            <label class="btn btn-default" data-toggle-class="btn-primary" data-toggle-passive-class="btn-default">
                               <input type="radio" name="gender" value="female"> Hombre
                             </label>
                           </div>
@@ -58,9 +58,8 @@
 
                       <div class="form-group">
                         <div class="col-md-9 col-sm-9 col-xs-12 col-md-offset-3">
-                        	<button type="button" class="btn btn-primary">Cancel</button>
-							<button class="btn btn-primary" type="reset">Reset</button>
-                        	<button type="submit" class="btn btn-success">Submit</button>
+							             <button class="btn btn-primary" type="reset">Limpiar formulario</button>
+                        	<button type="submit" class="btn btn-success">Crear nuevo usuario</button>
                         </div>
                       </div>
 
